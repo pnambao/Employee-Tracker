5ec07c8f3233e7437f757b4bddf73e207a4d1bda
RoachCoach <panjenambao@gmail.com>
2021-11-18 19:42:45 -0600

    modified db elements

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,14 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE department (
-id INT PRIMARY KEY,
-dep_name VARCHAR(30)
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+dep_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-id INT PRIMARY KEY,
-title VARCHAR(30),
-salary DECIMAL,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
 department_id INT,
 FOREIGN KEY (department_id)
 REFERENCES department(id)
@@ -19,10 +19,10 @@ ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-id INT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id INT,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
 FOREIGN KEY (role_id) REFERENCES role(id)
 ON DELETE SET NULL,
 manager_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,28 @@
-INSERT INTO department (id, dep_name)
-VALUES (001, "Engineering" ),
-       (002, "Sales"),
-       (003, "Finance"),
-       (004, "Legal Services");
+USE employeeTracker_db;
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (10, "Sales Lead", 100000, 002),
-       (20, "Salesperson", 80000, 002),
-       (30, "Lead Engineer", 150000, 001),
-       (40, "Software Engineer", 120000, 001),
-       (50, "Account Manager", 160000, 003),
-       (60, "Accountant", 125000, 003),
-       (70, "Legal Team Lead", 250000, 004),
-       (80, "Lawyer", 190000, 004);
+INSERT INTO department (dep_name)
+VALUES ("Engineering"),
+       ("Sales"),
+       ("Finance"),
+       ("Legal Services");
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "John", "Doe", 10, NULL),
-       (2, "Mike", "Chan", 20, 1),
-       (3, "Ashley", "Rodriguez", 30, NULL),
-       (4, "Kevin", "Tupik", 40, 3),
-       (5, "Kunal", "Singh", 50, NULL ),
-       (6, "Malia", "Brown", 60, 5),
-       (7, "Sarah", "Lourd", 70, NULL),
-       (8, "Tom", "Allen", 80, 7);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 2),
+       ("Salesperson", 80000, 2),
+       ("Lead Engineer", 150000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 10, NULL),
+       ("Mike", "Chan", 20, 1),
+       ("Ashley", "Rodriguez", 30, NULL),
+       ("Kevin", "Tupik", 40, 3),
+       ("Kunal", "Singh", 50, NULL ),
+       ("Malia", "Brown", 60, 5),
+       ("Sarah", "Lourd", 70, NULL),
+       ("Tom", "Allen", 80, 7);
        
